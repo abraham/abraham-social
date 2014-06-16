9efdcf45fca4ed0e1265b8eeb2c631f24540c08d
Abraham Williams <4braham@gmail.com>
2014-06-15 23:46:17 -0700

    added relative time

--- social-abrah-am/js/script.js
+++ social-abrah-am/js/script.js
@@ -1,3 +1,5 @@
+
+
 function addClass(existingClass, newClass) {
     var elements = document.getElementsByClassName(existingClass);
     for(var i = elements.length - 1; i > -1; --i){
@@ -33,3 +35,43 @@ _gaq.push(['_trackPageLoadTime']);
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
 })();
 
+function prettyLinks(){
+    var links = document.getElementsByClassName("time");
+    for ( var i = 0; i < links.length; i++ ){
+        if ( links[i].innerText ) {
+            var date = prettyDate(links[i].innerText);
+            if ( date ) {
+                links[i].innerText = date;
+            }
+        }
+    }
+}
+prettyLinks();
+setInterval(prettyLinks, 5000);
+
+/*
+ * JavaScript Pretty Date
+ * Copyright (c) 2011 John Resig (ejohn.org)
+ * Licensed under the MIT and GPL licenses.
+ */
+
+// Takes an ISO time and returns a string representing how
+// long ago the date represents.
+function prettyDate(time){
+  var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
+    diff = (((new Date()).getTime() - date.getTime()) / 1000),
+    day_diff = Math.floor(diff / 86400);
+      
+  if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
+    return;
+      
+  return day_diff === 0 && (
+      diff < 60 && "just now" ||
+      diff < 120 && "1 minute ago" ||
+      diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+      diff < 7200 && "1 hour ago" ||
+      diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+    day_diff == 1 && "Yesterday" ||
+    day_diff < 7 && day_diff + " days ago" ||
+    day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
+}
